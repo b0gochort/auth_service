ac1b5e9b4e12df432cac4ea3efc0343cb635dff9
Alexey <itbogochort@gmail.com>
2023-10-21 02:23:38 +0300

    add handler struct

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,4 +1,30 @@
 package handler
 
+import (
+	"fmt"
+
+	"github.com/b0gochort/internal/serivce"
+	"github.com/valyala/fasthttp"
+)
+
 type Handler struct {
+	services *serivce.Service
+}
+
+func NewHandler(services *serivce.Service) *Handler {
+	return &Handler{
+		services: services,
+	}
+
+}
+
+func (h *Handler) InitRoutes(ctx *fasthttp.RequestCtx) {
+	switch string(ctx.Path()) {
+	case "/ping":
+		ping(ctx)
+	}
+}
+
+func ping(ctx *fasthttp.RequestCtx) {
+	fmt.Println("pong")
 }
--- main.go
+++ main.go
@@ -3,44 +3,28 @@ package main
 import (
 	"fmt"
 
+	apidb "github.com/b0gochort/internal/api_db"
+	"github.com/b0gochort/internal/handler"
+	"github.com/b0gochort/internal/serivce"
 	"github.com/restream/reindexer/v3"
 	_ "github.com/restream/reindexer/v3/bindings/cproto"
 	"github.com/valyala/fasthttp"
 )
 
-var db = reindexer.NewReindex("cproto://rx.web-gen.ru:6534/tinkoff")
-
-func handleRequest(ctx *fasthttp.RequestCtx) {
-	switch string(ctx.Path()) {
-	case "/hello":
-		handleHello(ctx)
-	default:
-		handleNotFound(ctx)
-	}
-	// Отправляем ответ клиенту
-}
-
-// Обработка корневого маршрута "/"
-func handleRoot(ctx *fasthttp.RequestCtx) {
-	fmt.Fprintf(ctx, "Добро пожаловать на главную страницу!")
-}
-
-// Обработка маршрута "/hello"
-func handleHello(ctx *fasthttp.RequestCtx) {
-	fmt.Fprintf(ctx, "Привет, мир!")
-}
-
-// Обработка неизвестных маршрутов
-func handleNotFound(ctx *fasthttp.RequestCtx) {
-	ctx.Error("Страница не найдена", fasthttp.StatusNotFound)
-}
 func main() {
+	db := reindexer.NewReindex("cproto://rx.web-gen.ru:6534/tinkoff")
 	if db.Status().Err != nil {
 		panic(db.Status().Err)
 	}
 
+	apiDb := apidb.NewAPIDB(db)
+
+	service := serivce.NewService(apiDb)
+
+	handler := handler.NewHandler(service)
+
 	server := fasthttp.Server{
-		Handler: handleRequest, // Обработчик запросов
+		Handler: handler.InitRoutes, // Обработчик запросов
 	}
 
 	// Слушаем порт 8080 и обрабатываем запросы
