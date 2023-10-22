b867257f1702cbe319fb6f9cef5c417c2994c7d1
Uzer007-admin <nightfuriya@yandex.ru>
2023-10-22 10:42:39 +0300

    Update main.go

--- main.go
+++ main.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"fmt"
-	"github.com/spf13/viper"
 	"log"
+
+	"github.com/spf13/viper"
 
 	apidb "github.com/b0gochort/internal/api_db"
 	"github.com/b0gochort/internal/handler"
@@ -27,7 +28,7 @@ func main() {
 		SMTPPort string
 	}
 
-	db := reindexer.NewReindex("cproto://rx.web-gen.ru:6534/tinkoff")
+	db := reindexer.NewReindex("cproto://127.0.0.1:6534/tinkoff")
 	if db.Status().Err != nil {
 		panic(db.Status().Err)
 	}
@@ -45,7 +46,7 @@ func main() {
 	// Слушаем порт 8080 и обрабатываем запросы
 
 	fmt.Println("ok")
-	err := server.ListenAndServe(":8080")
+	err := server.ListenAndServe(":3301")
 	if err != nil {
 		fmt.Println("Ошибка при запуске сервера:", err)
 	}
